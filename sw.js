ee7d5133a64a51a0a99fa543076706e937561a19
birthofacorpseStudios <birthofacorpse@gmail.com>
2026-09-10 13:22:01 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-
-self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open('shifter-v47').then(c => c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png']))); });
-self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
